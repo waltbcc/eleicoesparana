176b993c32d86f53c26901c976f6a6823698cd45
Waltbcc <waltbcc@gmail.com>
2026-01-12 16:53:33 -0300

    prontoooo

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,9 @@ function App() {
   useEffect(() => {
     const fetchAuditoria = async () => {
       try {
-        const response = await axios.get(`/api/getAuditoria`);
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/api/getAuditoria`
+        );
         console.log("Auditoria:", response.data.rows);
         setAuditoria(response.data.rows);
       } catch (error) {
@@ -34,7 +36,9 @@ function App() {
   useEffect(() => {
     const fetchCapas = async () => {
       try {
-        const response = await axios.get(`/api/getCapas`);
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/api/getCapas`
+        );
         console.log("Capas:", response.data.rows);
         setCapas(response.data.rows);
       } catch (error) {
